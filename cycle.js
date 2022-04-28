625f9467e684fa96f1f54754ac08071bf885a5b2
Andrey <AndrEkat96@yandex.ru>
2022-04-28 13:04:23 +0500

    tasks CODE

---
+++ cycle.js
@@ -0,0 +1,32 @@
+/* 
+let i = 1;
+while (i <=100) {
+    console.log(i);
+    i++;
+}
+
+let i = 11;
+while (i <= 33){
+    console.log(i);
+    i++;
+}
+
+let i = 0;
+while (i <= 100) {
+    console.log(i);
+    i += 2;
+}
+
+let i = 1;
+while (i <= 99){
+    console.log(i);
+    i += 2;
+}
+
+let i = 30;
+while (i >= 0) {
+    console.log(i);
+    i --;
+}
+*/
+
