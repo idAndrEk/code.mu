0204e7bfffd3cdfb80222a34a64d52d64f3a5ee6
Andrey <AndrEkat96@yandex.ru>
2022-04-29 17:02:15 +0500

    tasks CODE

--- cycle.js
+++ cycle.js
@@ -28,5 +28,108 @@ while (i >= 0) {
     console.log(i);
     i --;
 }
+
+let i = 10;
+
+while (i >= 1) {
+    console.log(i);
+    i--;
+}
+
+let i = 10;
+
+while (i >= 1) {
+    console.log(i);
+    i--;
+}
+
+let i = 10;
+
+while (i >= 1) {
+    console.log(i);
+    i--;
+}
+
+let i = 10;
+
+while (i >= 1) {
+    console.log(i);
+    i--;
+}
+
+let i = 10;
+
+while (i >= 1) {
+    console.log(i);
+    i--;
+}
+
+for (let i = 1; i <= 100; i++) {
+    console.log(i);
+}
+
+for (let i = 11; i <= 33; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i <= 100; i += 2) {
+    console.log(i);
+}
+
+for (i = 1; i <= 99; i += 2) {
+    console.log(i);
+}
+
+let arr = ['a', 'b', 'c', 'd', 'e'];
+for ( i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
+
+let arr = [1, 2, 3, 4, 5];
+    	
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
+
+let arr = [1, 2, 3, 4, 5];
+
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 == 0) {
+        console.log(arr[i]);
+    }
+}
+
+let arr = [1, 2, 3, 4, 5];
+for (i = 0; i <= 5; i++) {
+    if (arr[i] % 2 - 1 == 0) {
+        console.log(arr[i])
+    }
+}
+
+let result = 0;
+let arr = [1, 2, 3, 4, 5];
+
+for (let i = 0; i < arr.length; i++) {
+	result += arr[i];
+}
+
+console.log(result);
+
+let result = 1;
+let arr = [1, 2, 3, 4, 5];
+for (let i = 0; i < arr.length; i++) {
+    result *= arr[i];
+}
+console.log(result);
 */
+
+
+let arr = [1, 2, 3, 4, 5];
+let result = 0;
+for (let i = 0; i < arr.length; i++) {
+    result += arr[i] * arr[i];
+}
+console.log((result));
+
+
 
